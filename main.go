e7dd127c52590b9b4f6be910504ac4e539376041
cotheo <theo.colliot@ynov.com>
2023-03-13 16:32:19 +0100

    Ajout du Header + Ajout des asset dans le main.go + debut de CSS pour la landing page

--- main.go
+++ main.go
@@ -13,13 +13,18 @@ func main() {
 
 	files := []string{
 		"templates/index.html",
-		"templates/fiche-perso.html"}
+		"templates/fiche-perso.html",
+		"templates/header.html"}
 
 	tmpl := template.Must(template.ParseFiles(files...))
 
 	// Ajout du CSS
-	fs := http.FileServer(http.Dir("style"))
+	fs := http.FileServer(http.Dir("./style"))
 	http.Handle("/style/", http.StripPrefix("/style/", fs))
+
+	// Ajout des images
+	fs_deux := http.FileServer(http.Dir("./src/assets"))
+	http.Handle("/src/assets/", http.StripPrefix("/src/assets/", fs_deux))
 
 	fmt.Println("(http://localhost:8080)", "Server started on port ", port)
 
